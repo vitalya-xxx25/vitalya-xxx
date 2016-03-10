789863652fea15c56cd1d1d652437d39e278fc9e
Виталий <vitaly.golovin25@gmail.com>
2016-03-10 14:03:08 +0300

    install adminLte

--- backend/config/main.php
+++ backend/config/main.php
@@ -13,6 +13,13 @@ return [
     'bootstrap' => ['log'],
     'modules' => [],
     'components' => [
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                   '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
+                ],
+            ],
+        ],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,

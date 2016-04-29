182628b0ec2285b916d63edcf4e3ca7a8cdd441e
Виталий <vgolovin@lightsoft.ru>
2016-04-29 16:52:17 +0300

    test commit 3

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -54,13 +54,13 @@ class SiteController extends Controller
      */
     public function actions(){
         return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
+				'error' => [
+					'class' => 'yii\web\ErrorAction',
+				],
+				'captcha' => [
+					'class' => 'yii\captcha\CaptchaAction',
+					'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+				],
         ];
     }
 
